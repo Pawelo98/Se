a40da0efce7b14229eefdf4d84fd858437161c4c
Pawelo98 <36399619+Pawelo98@users.noreply.github.com>
2018-06-28 03:18:39 +0200

    Delete Iterator.java

--- Iterator.java
+++
@@ -1,9 +0,0 @@
-public interface Iterator<Pracownik>{
-
-    public void poprzedni();
-    public void nastepny();
-    public void pierwszy();
-    public void ostatni();
-    public boolean jestWZakresie();
-    public Pracownik elementBiezacy();
-}
